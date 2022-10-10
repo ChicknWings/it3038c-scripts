b1ee5fbd9a9fe2d907a4586851418324d46d496c
Shaoran <yang2sr@mail.uc.edu>
2022-10-09 23:20:19 -0400

    Project 1

---
+++ project1/GuessNumber/GuessNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            int number = rd.Next(1, 101);
+
+            while (true) 
+            {
+                Console.WriteLine("Guess a number between 1-100");
+                int numberUser = Convert.ToInt32(Console.ReadLine());
+                if (numberUser > number)
+                {
+                    Console.WriteLine("My number is smaller");
+                }
+                else if (numberUser < number) 
+                {
+                    Console.WriteLine("My number is bigger");
+                }
+                else
+                {
+                    Console.WriteLine("Yeah! This is my number!");
+                    break;
+                }
+
+
+
+            }
+
+        }
+    }
+}
